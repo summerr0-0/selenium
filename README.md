30b74f691c15d7794a688e36f973a6b569ab20dc
정이린(jeong_irin) <jeong_irin@kolon.com>
2023-09-20 18:05:25 +0900

    selenium example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # selenium
 selenium test practice
+
+[셀레니움 doc](https://www.selenium.dev/)
+
+python 3.9
+selenium 4.12.0
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 
 import pyautogui
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 
 
 def print_hi():
@@ -16,32 +18,56 @@ def print_hi():
 
     # 페이지 이동
     driver.get("")
-    driver.get("")
     # 브라우저 정보
     print("driver.title : ", driver.title)
 
     # 요소 찾기
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    id_box = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[1]/div/div/span/span/input")
+    password_box = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[2]/div/div/span/span/input")
+    phone_box = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[3]/div/div/span/span/input")
+    submit_button = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[3]/div/div/span/button")
+    login_button = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[5]/div/div/span/button")
+    # driver.find_element(By.XPATH, '//button[text()="Some text"]')
+    # driver.find_element(By.XPATH, '//button')
+    # driver.find_element(By.ID, 'loginForm')
+    # driver.find_element(By.LINK_TEXT, 'Continue')
+    # driver.find_element(By.PARTIAL_LINK_TEXT, 'Conti')
+    # driver.find_element(By.NAME, 'username')
+    # driver.find_element(By.TAG_NAME, 'h1')
+    # driver.find_element(By.CLASS_NAME, 'content')
+    # driver.find_element(By.CSS_SELECTOR, 'p.content'
 
-    # 대기
-    driver.implicitly_wait(3)
 
     # 대기전략(실패)
-    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, "message"))
+    # WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, "message"))
 
     # 요소에 대한 조치
-    text_box.send_keys("Selenium")
-    sleep(2)
+    id_box.send_keys("")
+    password_box.send_keys("")
+    phone_box.send_keys("")
+    time.sleep(1)
     submit_button.click()
+    time.sleep(3)
 
-    # 대기전략(성공)
-    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, "message"))
+    # 대기
+    certification_number_box = driver.find_element(by=By.XPATH, value="/html/body/div/div/form/div[4]/div/div/span/span/input")
+    certification_number_box.send_keys("")
+
+    time.sleep(3)
+    login_button.send_keys(Keys.ENTER)
+
+    # # 대기
+
+    driver.implicitly_wait(60)
+
+
 
     # element 정보 요청
-    message = driver.find_element(by=By.ID, value="message")
-    value = message.text
-    pyautogui.alert(text='', title='', button='OK')
+    # message = driver.find_element(by=By.ID, value="message")
+    # value = message.text
+    # pyautogui.alert(text='', title='', button='OK')
+
+
 
     # 세션 종료
     driver.quit()
